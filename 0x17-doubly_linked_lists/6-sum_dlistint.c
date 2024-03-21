6940cb153a4e88d2d323b6b6838288d4780b8a05
El Idrissi Mustapha <appsbymuss@gmail.com>
2024-03-21 13:32:55 -0700

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
-
 /**
- * sum_dlistint - sum of elements in list
- * @head: list
- * Return: int
- */
-
+  * sum_dlistint - sums all nodes in a linked list
+  * @head: head of the list
+  * Return: the sum of the nodes
+  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
-	while (head != NULL)
+	int sum;
+
+	while (head)
 	{
 		sum += head->n;
 		head = head->next;
